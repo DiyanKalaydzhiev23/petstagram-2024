aec98adf8104e8be1619dcf72ca697030a3389c9
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2024-09-24 20:59:09 +0300

    feat: added photo model and validator

--- petstagram/petstagram/photos/models.py
+++ petstagram/petstagram/photos/models.py
@@ -1,3 +1,36 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
+from petstagram.pets.models import Pet
+from petstagram.photos.validators import FileSizeValidator
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(
+        validators=[
+            FileSizeValidator(5),
+        ],
+    )
+
+    description = models.TextField(
+        max_length=300,
+        validators=(
+            MinLengthValidator(10),
+        ),
+        blank=True,
+        null=True,
+    )
+
+    location = models.CharField(
+        max_length=30,
+        blank=True,
+        null=True,
+    )
+
+    tagged_pets = models.ManyToManyField(
+        to=Pet,
+        blank=True,
+    )
+
+    date_of_publication = models.DateField(
+        auto_now_add=True,
+    )
---
+++ petstagram/petstagram/photos/validators.py
@@ -0,0 +1,29 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+# def validate_size(value):
+#     if value.size > 5 * 1024 * 1024:
+#         raise ValidationError('File is too big')
+#
+
+@deconstructible
+class FileSizeValidator:
+    def __init__(self, file_size_mb: int, message=None):
+        self.file_size_mb = file_size_mb
+        self.message = message
+
+    @property
+    def message(self):
+        return self.__message
+
+    @message.setter
+    def message(self, value):
+        if value is None:
+            self.__message = f"File size must be below or equal to {self.file_size_mb}MB"
+
+        self.__message = value
+
+    def __call__(self, value):
+        if value.size > self.file_size_mb * 1024 * 1024:
+            raise ValidationError(self.message)
